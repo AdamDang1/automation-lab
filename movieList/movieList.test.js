d4b39ca8964b88f9a306fb3d9e76a426056f369f
AdamDang1 <dtdang1@gmail.com>
2022-04-30 17:05:24 -0500

    not done yet just taking a break but got one test done

--- movieList/movieList.test.js
+++ movieList/movieList.test.js
@@ -4,10 +4,17 @@ require('chromedriver');
 
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build();
 
+const { addMovie } = require('./functions/movieFunctions');
+
 beforeAll(async () => {
-    await driver.get('http://localhost:5500/movieList/index.html');
+    await driver.get('http://127.0.0.1:5500/movieList/index.html');
 });
 
 afterAll(async () => {
-    await driver.quit()
+    await driver.sleep(1000);
+    await driver.quit();
+});
+
+it('should add new movie to list', async () => {
+    await addMovie(driver);
 });
